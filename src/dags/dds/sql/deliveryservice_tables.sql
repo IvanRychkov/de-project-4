cc800a2de489139ce4755e3f462d2273d8c088c0
Ivan Rychkov <rychkov.ivan@gmail.com>
2023-06-04 21:02:35 +0300

    reformat file

--- src/dags/dds/sql/deliveryservice_tables.sql
+++ src/dags/dds/sql/deliveryservice_tables.sql
@@ -6,10 +6,10 @@ from stg.deliverysystem_couriers;
 
 truncate table dds.fct_delivery;
 insert into dds.fct_delivery (delivery_id, courier_id, order_id, rating, tip_amt)
-select object_id               as delivery_id,
-       c.id                    as courier_id,
-       o.id                    as order_id,
-       (object_value ->> 'rate')::smallint as rating,
+select object_id                                    as delivery_id,
+       c.id                                         as courier_id,
+       o.id                                         as order_id,
+       (object_value ->> 'rate')::smallint          as rating,
        (object_value ->> 'tip_sum')::numeric(14, 2) as tip_amt
 
 from stg.deliverysystem_deliveries d
